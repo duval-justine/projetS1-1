c6ace2e308d9e05c3ffe60fea92b7a7529dac983
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-01-05 11:56:50 +0100

    Modification value du carousel 1 pour combler l'espace vide

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -15,8 +15,8 @@ function carrousel1() {
 
     anim1.add({
         targets: images1,
-        translateX: [{value: '500', duration: d},
-                     {value: '-500', duration: 0, delay: function(img,ind) {
+        translateX: [{value: '197', duration: d},
+                     {value: '-197', duration: 0, delay: function(img,ind) {
                          if (ind == 0) return (3*delta)+(2*d);
                          if (ind == 1) return (2*delta)+d;
                          if (ind == 2) return delta;
