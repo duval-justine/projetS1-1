5fa172e81a9291a64f3e617dfec3390f537f3166
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-01-05 11:42:40 +0100

    Création du carousel de l'article 2

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -35,3 +35,50 @@ function carrousel1() {
         }
     });
 }
+
+function carrousel2() {
+	var images2 = document.querySelectorAll('.image2');
+    let d = 2000;
+    let delta = 500;
+
+    images2.forEach(function(img,indice) {
+        img.style.zIndex = images2.length - indice;
+    });
+
+    let anim2 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim2.add({
+        targets: images2,
+        translateX: [{value: '500', duration: d},
+                     {value: '-500', duration: 0, delay: function(img,ind) {
+                        if (ind == 0) return (8*delta)+(7*d);
+                        if (ind == 1) return (7*delta)+(6*d);
+                        if (ind == 2) return (6*delta)+(5*d);
+                        if (ind == 3) return (5*delta)+(4*d);
+                        if (ind == 4) return (4*delta)+(3*d);
+                        if (ind == 5) return (3*delta)+(2*d);
+                        if (ind == 6) return (2*delta)+d;
+                        if (ind == 7) return delta;
+                        return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 8) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            if (ind == 3) return (4*delta)+(3*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            if (ind == 7) return (8*delta)+(7*d);
+            return (10*delta)+(9*d);
+        }
+    });
+}
